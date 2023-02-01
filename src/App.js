e3bc772544fe579814957a0a75ad1eea5ab2ddb4
Morgan Brown <mcbrown1890@gmail.com>
2023-02-01 00:52:56 -0500

    UPDATE AND DELETE ADDED

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Main from './views/Main';
 import Detail from './components/Detail';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Update from './components/Update';
 const App = () => {
     
   return(
@@ -11,6 +12,7 @@ const App = () => {
       <Routes>
         <Route element={<Main/>} path="/home" default /> 
         <Route element={<Detail/>} path="/products/:id" />
+        <Route element={<Update/>} path="/products/edit/:id"/>
       </Routes>
     </BrowserRouter>
       </div>
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,22 +2,18 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios';
 import {Link} from 'react-router-dom';
 const ProductList = (props) => {
-    const {products, setProducts} = props;
-    useEffect(() => {
-        axios.get("http://localhost:8000/api/products")
-        .then((res) => {
-            console.log(res.data);
-            setProducts(res.data);
+    const {removeFromDom, products, setProducts} = props;
+    const deleteProduct = (productId) => {
+        axios.delete('http://localhost:8000/api/products' + productId)
+            .then(res => {
+                removeFromDom(productId)
             })
-        .catch((err) => {
-            console.log(err);
-        })
-    });
-
+            .catch(err => console.log(err))
+    }
     return (
         <div>
             {products.map((product, index) => {
-                return <div key={index}> <p>{product.title}, {product.price}, {product.description}, <Link to={`/products/${product._id}`}> {product.title} details </Link></p></div>
+                return <div key={index}> <p>{product.title}, {product.price}, {product.description}, <Link to={`/products/${product._id}`}> {product.title} details </Link> <Link to={`/products/edit/${product._id}`}> Edit {product.title}</Link></p><button onClick={(e)=>{deleteProduct(product._id)}}>DELETE</button></div>
 })} </div>);}
 
 export default ProductList;
